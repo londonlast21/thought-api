56c1f9da0643cd614f13a3a714568356c9031013
jack <biffisbuffm@gmail.com>
2020-09-26 12:03:45 -0500

    controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,9 +28,39 @@ const userController = {
             });
           
     },
-   
 
-   
+    // POST route to create user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // PUT route to update user by their ID
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbUserData => {
+                if  (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this ID' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE route to delete user by ID
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 }
 
 module.exports = userController;
