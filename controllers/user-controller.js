a93bb0adebfc7705ee6cd26a09852fe014c2611d
jack <biffisbuffm@gmail.com>
2020-09-26 11:36:37 -0500

    get routes for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,12 +4,6 @@ const userController = {
     // GET all users
     getAllUser(req, res) {
         User.find({})
-            //populate with comments when set up
-            // .populate({
-
-            // })
-            .select('_v')
-            .sort({ _id: -1 })
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
@@ -17,11 +11,26 @@ const userController = {
             });
     },
 
-    // GET single user with ID
-    getUserById({ params }, res) {
+    // GET one user by ID
+    getuserById({ params }, res) {
         User.findOne({ _id: params.id })
-        // populate with comments when set up
-        
-            .
-    }
+            .then(dbUserData => {
+                // if no user found send 404
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this ID '});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+          
+    },
+   
+
+   
 }
+
+module.exports = userController;
