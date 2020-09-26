9a7610c05f781fbd22438eebad9911fbcbfe7252
jack <biffisbuffm@gmail.com>
2020-09-26 18:49:51 -0500

    fix branch for bad answer from instruction

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Thought = require('./Thought');
+const Reaction = require('./Thought');
 
 
-module.exports = { User, Thought };
+module.exports = { User, Thought, Reaction };
