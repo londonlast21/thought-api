650adfc8170150b1364c66e490e115cb52997580
jack <biffisbuffm@gmail.com>
2020-09-26 10:56:26 -0500

    server works except /routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
+//app.use(require('./routes'));
 
 // log mongo queries
 mongoose.set('debug', true);
