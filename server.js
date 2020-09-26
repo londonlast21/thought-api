b50bda76fefc4aefd451c56394c164b0d06dec99
jack <biffisbuffm@gmail.com>
2020-09-26 10:53:53 -0500

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+// server 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+// log mongo queries
+mongoose.set('debug', true);
+
+// start
+app.listen(PORT, () => console.log(`Now connected on localhost:${PORT}`));
